0357fddb1f4a8c35a8283aee039521682e44c6a7
kedor <99351777+aure31@users.noreply.github.com>
2025-10-10 19:16:21 +0200

    chore : add panda3d and the environement

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-import pygame
+from direct.showbase.ShowBase import ShowBase
 
+
+class HarePoonApp(ShowBase):
+
+    def __init__(self):
+        ShowBase.__init__(self)
+
+
+app = HarePoonApp()
+app.run()
