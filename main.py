5c5ade4f1182e76056473e3447436f8c7003ea72
Sertraln <djipoudou@gmail.com>
2025-11-11 22:56:07 +0100

    Déplacements basiques + camera fps

--- main.py
+++ main.py
@@ -1,59 +1,19 @@
-from direct.showbase.ShowBase import ShowBase
-from direct.task import Task
-from panda3d.core import CardMaker, Vec3,WindowProperties
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
 
-class HarePoonApp(ShowBase):
+app = Ursina()
 
-    def __init__(self):
-        ShowBase.__init__(self)
-        # Load the environment model.
+ground = Entity(
+    model='plane',
+    scale=(100,1,100),
+    texture='grass',
+    texture_scale=(10,10),
+    collider='box')
 
-        self.center_mouse()
+player = FirstPersonController(
+    model='sphere',
+    color=color.blue,
+    scale_y=2,
+    position=(0,1,0))
 
-        cm = CardMaker("plane")
-        cm.setFrame(-1, 1, -1, 1)  # taille du plan (gauche, droite, bas, haut)
-
-        plane = self.render.attachNewNode(cm.generate())
-        plane.setPos(0, 5, 0)       # position dans la scène
-        plane.setHpr(0, -90, 0)     # rotation pour qu’il soit horizontal (face vers le haut)
-        plane.setScale(2)           # mise à l’échelle
-
-        # Optionnel : ajout d’une couleur
-        plane.setColor(0.5, 0.8, 0.5, 1)
-        plane.setTwoSided(True)
-        player = self.loader.loadModel("models/box")
-        player.reparent_to(self.render)
-        player.setPos(0,5,0)
-        player.setColor(0.2,0.6,0.9, 1)
-        player.setScale(1,1,2)
-        
-        self.cam.setPos(0, -1, 5)
-        self.cam.lookAt(plane) 
-        # Apply scale and position transforms on the model.
-        self.taskMgr.add(self.cameraRotation,"RotationControlTask")
-        self.taskMgr.add(self.movement,"positionControlTask")
-
-    def center_mouse(self):
-        props = WindowProperties()
-        props.set_cursor_hidden(True)
-        props.setMouseMode(WindowProperties.M_relative)
-        self.win.requestProperties(props)
-
-    def cameraRotation(self, task):
-        if self.mouseWatcherNode.hasMouse():
-            x = self.mouseWatcherNode.getMouseX()
-            y = self.mouseWatcherNode.getMouseY()
-            xRot = x * 6.0
-            yRot = y * 6.0
-            print(f"Mouse pos: {x}, {y}")
-            self.camera.set_hpr(xRot, yRot,0)
-
-        return Task.cont
-
-    def movement(self,task):
-        return Task.cont
-
-
-
-app = HarePoonApp()
 app.run()
