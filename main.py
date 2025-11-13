183b396df7ea2755760d051efcb1f265152c1252
AMR999X <joel.lipski2007@gmail.com>
2025-11-13 22:42:51 +0100

    Ajout de 3eme personne + changements dans mouvement + shift to run

--- main.py
+++ main.py
@@ -3,17 +3,48 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
 
+# Create ground
 ground = Entity(
-    model='plane',
+    model='cube',
     scale=(100,1,100),
     texture='grass',
     texture_scale=(10,10),
     collider='box')
 
+# Create player
 player = FirstPersonController(
-    model='sphere',
-    color=color.blue,
-    scale_y=2,
-    position=(0,1,0))
+    position=(0,4,0),
+    jump_height = 8,
+    gravity = 2.5)
+
+# Set cursor white cause pink ugly af
+player.cursor.color = color.white
+
+# Create its shape (body)
+body = Entity(
+    parent=player,
+    model='cube',
+    scale=(1,1.5,1),
+    y=0.75
+)
+
+# Set third person
+camera.z = -5
+
+# Set basic sky
+Sky(color=color.violet)
+
+def update():
+    # Make player respawn if he falls
+    if player.y < -20:
+        player.position = (0,4,0)
+    
+    # Sprints if shift is held
+    if held_keys['shift']:
+        body.color = color.red
+        player.speed = 20
+    else:
+        body.color = color.blue
+        player.speed = 10
 
 app.run()
