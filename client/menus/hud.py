6a489ccfe9a40ce62910f3c62f5ad7d2de2179b5
Joel Lipski <joel.lipski2007@gmail.com>
2026-05-26 16:21:20 +0200

    quit in dialogue added and scales moved

--- client/menus/hud.py
+++ client/menus/hud.py
@@ -1,5 +1,5 @@
 import client.menu as menu
-from ursina import Text
+from ursina import Text, Entity
 import client.data as data
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
@@ -8,9 +8,10 @@ if TYPE_CHECKING:
 class Hud(menu.Menu):
     def __init__(self):
         super().__init__("hud")
-        self.currency_text = Text(parent=self, text="<image:assets/textures/fish_scale>: 0", position=(0.5, 0.4), origin=(0, 0.5), scale=2, font=data.fisho_font)
+        self.scales = Entity(model="quad", texture="assets/textures/fish_scale.png", parent=self, position=(0.3, 0.4), scale=0.1)
+        self.currency_text = Text(parent=self, text=": 0", position=(0.5, 0.4), origin=(0, 0.5), scale=2, font=data.fisho_font)
 
     def update_currency(self, amount: int):
-        self.currency_text.text = f"<image:assets/textures/fish_scale>: {amount}"
+        self.currency_text.text = f": {amount}"
 
 data.hud = Hud()
--- client/shop.py
+++ client/shop.py
@@ -64,16 +64,21 @@ class UpgradeMenu(menu.Menu):
         self.btn_back.on_click = lambda: menu.show("shop")
         self.add_element(self.btn_back)
 
+def hide_dialogue():
+    menu.hide()
+    mouse.locked = True
+
 class ShopMenu(menu.Menu):
     def __init__(self):
         super().__init__("shop", pause=True)
         self.bg = Entity(parent=self, model='quad', color=color.rgba(0, 0, 0, 0.5), scale=(1.3, 0.4), position=(0, -0.3), z=0)
         self.npc_text_a = Text(text="Hey, t'as des poissons pour moi ?", color=color.white, position=(-0.6, -0.15), scale=1.5, font=data.fisho_font)
         self.npc_text_b = Text(text="Je commence à avoir faim...", color=color.white, position=(-0.6, -0.22), scale=1.5, font=data.fisho_font)
-        self.btn_sell = FixedButton(text="Vendre tous les poissons - 0$", color=color.white, position=(-0.6, -0.35), scale=(0.3, 0.1), origin=(-0.5, 0))
-        self.btn_buy = FixedButton(text="Acheter une amélioration", color=color.white, position=(-0.6, -0.40), scale=(0.3, 0.1), origin=(-0.5, 0))
+        self.btn_sell = FixedButton(text="Vendre tous les poissons - 0$", color=color.white, position=(-0.57, -0.33), scale=(0.3, 0.1), origin=(-0.5, 0))
+        self.btn_buy = FixedButton(text="Acheter une amélioration", color=color.white, position=(-0.6, -0.4), scale=(0.3, 0.1), origin=(-0.5, 0))
+        self.btn_quit = FixedButton(text="Quitter", color=color.white, position=(-0.72, -0.47), scale=(0.3, 0.1), origin=(-0.5, 0))
 
-        for btn in [self.npc_text_a, self.npc_text_b, self.btn_sell, self.btn_buy]:
+        for btn in [self.npc_text_a, self.npc_text_b, self.btn_sell, self.btn_buy, self.btn_quit]:
             btn.origin = (-0.5, 0)
             self.add_element(btn)
             btn.set_bin('fixed', 1)
@@ -82,6 +87,7 @@ class ShopMenu(menu.Menu):
 
         self.btn_sell.on_click = self.sell_fish
         self.btn_buy.on_click = self.buy_upgrade
+        self.btn_quit.on_click = hide_dialogue
 
     def update_sell_button(self):
         total = 0
